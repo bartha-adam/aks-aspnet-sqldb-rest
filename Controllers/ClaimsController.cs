b944ef29830a8602d4cf46cacbfa487b6c93ec08
Adam Bartha <adam.bartha@macadamian.com>
2019-11-27 14:30:24 +0200

    another log

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -43,7 +43,7 @@ namespace ClaimsApi.Controllers
             }
             else
             {
-                _logger.LogInformation("SKIP Creating test claim data!!");
+                _logger.LogInformation("SKIP Creating test claim data!!!");
             }
         }
 
